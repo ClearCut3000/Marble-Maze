d2be88caffeb976b78cbeff95e58721b86e9fd9b
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-02-06 23:41:19 +0300

    Contacting but not colliding

--- Marble Maze/GameScene.swift
+++ Marble Maze/GameScene.swift
@@ -8,12 +8,19 @@
 import SpriteKit
 import CoreMotion
 
-final class GameScene: SKScene {
+final class GameScene: SKScene, SKPhysicsContactDelegate {
 
   //MARK: - Properties
   private var player: SKSpriteNode!
   private var lastTouchPosition: CGPoint?
   private var motionManager: CMMotionManager?
+  private var scoreLabel: SKLabelNode!
+  private var score = 0 {
+    didSet {
+      scoreLabel.text = "Score: \(score)"
+    }
+  }
+  private var isGameOver = false
 
   //MARK: - Scene
   override func didMove(to view: SKView) {
@@ -22,14 +29,22 @@ final class GameScene: SKScene {
     background.blendMode = .replace
     background.zPosition = -1
     addChild(background)
+    scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+    scoreLabel.text = "Score: 0"
+    scoreLabel.horizontalAlignmentMode = .left
+    scoreLabel.position = CGPoint(x: 16, y: 16)
+    scoreLabel.zPosition = 2
+    addChild(scoreLabel)
     loadLevel()
     createPlayer()
     physicsWorld.gravity = .zero
+    physicsWorld.contactDelegate = self
     motionManager = CMMotionManager()
     motionManager?.startAccelerometerUpdates()
   }
 
   override func update(_ currentTime: TimeInterval) {
+    guard isGameOver == false else { return }
     #if targetEnvironment(simulator)
     if let lastTouchPosition = lastTouchPosition {
       let diff = CGPoint(x: lastTouchPosition.x - player.position.x, y: lastTouchPosition.y - player.position.y)
@@ -42,7 +57,7 @@ final class GameScene: SKScene {
     #endif
   }
 
-  //MARK: - Methods
+  //MARK: - UIMethods
   private func loadLevel() {
     guard let levelURL = Bundle.main.url(forResource: "level1", withExtension: "txt") else {
       fatalError("Could't find level.txt in the app bundle!")
@@ -112,6 +127,38 @@ final class GameScene: SKScene {
     player.physicsBody?.contactTestBitMask = CollisionTypes.star.rawValue | CollisionTypes.vortex.rawValue | CollisionTypes.finish.rawValue
     player.physicsBody?.collisionBitMask = CollisionTypes.wall.rawValue
     addChild(player)
+  }
+
+  //MARK: - SKPhysicsContactDelegate Methods
+  func didBegin(_ contact: SKPhysicsContact) {
+    guard let nodeA = contact.bodyA.node else { return }
+    guard let nodeB = contact.bodyB.node else { return }
+    if nodeA == player {
+      playerCollided(with: nodeB)
+    } else if nodeB == player {
+      playerCollided(with: nodeA)
+    }
+  }
+
+  private func playerCollided(with node: SKNode) {
+    if node.name == "vortex" {
+      player.physicsBody?.isDynamic = false
+      isGameOver = true
+      score -= 1
+      let move = SKAction.move(to: node.position, duration: 0.5)
+      let scale = SKAction.scale(to: 0.0001, duration: 0.25)
+      let remove = SKAction.removeFromParent()
+      let sequence = SKAction.sequence([move, scale,remove])
+      player.run(sequence) { [weak self] in
+        self?.createPlayer()
+        self?.isGameOver = false
+      }
+    } else if node.name == "ster" {
+      node.removeFromParent()
+      score += 1
+    } else if node.name == "finish" {
+      
+    }
   }
 
   //MARK: - Touches Methods
